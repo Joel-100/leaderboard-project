499d5ebb19365b45a0438de932bef131e78f8d70
Joel-100 <joelkugonza@gmail.com>
2022-10-12 20:10:45 +0300

    Add page layout and styling

---
+++ dist/index.bundle.js
@@ -0,0 +1,18 @@
+(self["webpackChunkwebpack_template"] = self["webpackChunkwebpack_template"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
